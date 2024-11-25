ce7cf5024c5a289ab21022182edfb31aa5bfbce5
park <dall011@naver.com>
2024-11-25 22:43:03 +0900

    [24/11/22] JPA 연동

--- db/src/main/java/com/start/db/repository/account/MemberRepository.java
+++ db/src/main/java/com/start/db/repository/account/MemberRepository.java
@@ -3,7 +3,12 @@ package com.start.db.repository.account;
 import com.start.db.entity.account.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByUserId(String userId);
+
+    List<Member> findAllByIdExists(String id);
+
 
 }
